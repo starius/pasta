bc28a795d52ef4e998a4f77b15aeb3ad8503e1ba
Boris Nagaev <bnagaev@gmail.com>
2023-05-20 19:19:58 +0200

    main page: remove non-working go get command

--- gopasta/server/templates.go
+++ gopasta/server/templates.go
@@ -117,7 +117,7 @@ Enter URL:
 {{ end }}
 
 <br/><br/>
-Get the <a href="https://github.com/starius/pasta/tree/master/gopasta">source</a> of the site and install on your own server in 3 seconds: <code>go get github.com/starius/pasta/gopasta</code> .
+<a href="https://github.com/starius/pasta/tree/master/gopasta">How to setup your own instance</a>
 
 </body></html>`))
 
